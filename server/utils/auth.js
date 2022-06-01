cdfec96cba2f1b9e5dcb3acd4aca6a0adff42bd7
JulesMcP <rogue427@yahoo.com>
2022-06-01 11:55:38 -0500

    adds schema file structure

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = 'donttellnobody';
 const expiration = '2h';
 
 module.exports = {
